1fad7749b9cd5b04d2ea8d9c19271c8d7df04a76
MathiasAAU <mkra17@student.aau.dk>
2022-05-10 14:58:28 +0200

    getlevelcolor

--- src/lib/utils/levelIcon.ts
+++ src/lib/utils/levelIcon.ts
@@ -5,3 +5,33 @@ export function getLevelIcon(level: number): string {
 export function getWCAGMasterIcon(level: number): string {
     return `/static/WCAGMaster/${level}.png`
 }
+
+export function getLevelColor(level: number): string {
+    if (level <= 5) {
+        return '#74C8DE'
+    } else if (level <= 7) {
+        return '#5DABFF'
+    } else if (level <= 9) {
+        return '#6583FF'
+    } else if (level <= 19) {
+        return '#53BE78'
+    } else if (level <= 29) {
+        return '#26A69A'
+    } else if (level <= 39) {
+        return '#A088E4'
+    } else if (level <= 49) {
+        return '#D66AD3'
+    } else if (level <= 59) {
+        return '#D1D66B'
+    } else if (level <= 69) {
+        return '#C99646'
+    } else if (level <= 79) {
+        return '#FF700B'
+    } else if (level <= 89) {
+        return '#DE1848'
+    } else if (level <= 99) {
+        return '#187DDE'
+    } else {
+        return '#FFDE94'
+    }
+}
