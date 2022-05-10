58abd8a73a9fa13f45d47a7e03fe520a2293d35e
Jens Andersen <jensandersen@hey.com>
2022-05-10 14:13:30 +0200

    Add profile page

---
+++ src/routes/profile/[id].svelte
@@ -0,0 +1,13 @@
+<script>
+  // your script goes here
+
+  import Card from '$lib/components/progressCard/Card.svelte'
+</script>
+
+<!-- markup (zero or more items) goes here -->
+
+<h1>hello</h1>
+
+<div class="w-30">
+  <Card />
+</div>
