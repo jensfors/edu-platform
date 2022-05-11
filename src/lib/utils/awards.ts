4376ea8faa0d1b221570938a134780b6898618c4
MathiasAAU <mkra17@student.aau.dk>
2022-05-11 12:46:04 +0200

    Added comments and new master images

--- src/lib/utils/awards.ts
+++ src/lib/utils/awards.ts
@@ -1,9 +1,9 @@
 import type { WCAGCriteria } from "@prisma/client";
 import { getPrincipleNum, type Principle } from "./stringTypes";
 
-export const maxWCAGMaster: number = 5
-export const maxPrincipleLevel: number = 100
+export const maxMasterLevel: number = 5
 
+// Adds solves to criteria
 export function addSolvesToCriteria(allCriteria: WCAGCriteria[], solvedCriteria: WCAGCriteria[]) {
     allCriteria.forEach((criterion: WCAGCriteria) => {
         let isSolved: boolean = false
@@ -21,19 +21,23 @@ export function addSolvesToCriteria(allCriteria: WCAGCriteria[], solvedCriteria:
     })
 }
 
+// Gets the users WCAG Master level
 export function getWCAGMasterLevel(criteria: WCAGCriteria[]): number {
-    return getMinimumSolves(criteria, maxWCAGMaster)
+    return getMinimumSolves(criteria, maxMasterLevel)
 }
 
+// Gets the next wcag Master level for a user (max = maxWCAGMaster)
 export function GetNextMasterLevel(masterLevel: number): number {
-    return masterLevel > maxWCAGMaster ? maxWCAGMaster : masterLevel + 1
+    return masterLevel > maxMasterLevel ? maxMasterLevel : masterLevel + 1
 }
 
+// Gets the masterlevel for a principle
 export function getPrincipleMasterLevel(criteria: WCAGCriteria[], principle: Principle): number {
     const principleCriteria: WCAGCriteria[] = getPrincipleCriteria(criteria, getPrincipleNum(principle))
-    return getMinimumSolves(principleCriteria, maxPrincipleLevel)
+    return getMinimumSolves(principleCriteria, maxMasterLevel)
 }
 
+// Gets all criteria for a principle 
 function getPrincipleCriteria(criteria: WCAGCriteria[], principleNumber: number): WCAGCriteria[] {
     let result: WCAGCriteria[] = []
     criteria.forEach((criterion) => {
@@ -43,6 +47,7 @@ function getPrincipleCriteria(criteria: WCAGCriteria[], principleNumber: number)
     return result
 }
 
+// Finds the minimum solves of a list of criteria
 function getMinimumSolves(criteria: WCAGCriteria[], maxLevel: number): number {
     let level = maxLevel
     criteria.forEach((criterion: WCAGCriteria) => {
@@ -52,16 +57,18 @@ function getMinimumSolves(criteria: WCAGCriteria[], maxLevel: number): number {
     return level
 }
 
-
+// Returns the progress for Master level
 export function getMasterProgress(masterLevel: number, criteria: WCAGCriteria[]): { progress: number, total: number } {
-    const progress: number = getTotalSolvesForMasterLevel(masterLevel, criteria)
-    const total: number = criteria.length * (masterLevel < maxWCAGMaster ? masterLevel + 1 : maxWCAGMaster)
+    let progress: number = getTotalSolvesForMasterLevel(masterLevel, criteria)
+    const total: number = criteria.length * (masterLevel < maxMasterLevel ? masterLevel + 1 : maxMasterLevel)
+    progress = progress > total ? total : progress
     return { progress, total }
 }
 
+// returns the progress of solves for a certain master level
 function getTotalSolvesForMasterLevel(masterLevel: number, criteria: WCAGCriteria[]): number {
     let solves: number = 0
-    let nextMasterLevel: number = masterLevel === maxWCAGMaster ? maxWCAGMaster : masterLevel + 1
+    let nextMasterLevel: number = masterLevel === maxMasterLevel ? maxMasterLevel : masterLevel + 1
     criteria.forEach((criterion) => {
         // @ts-ignore
         solves += criterion.solves > nextMasterLevel ? nextMasterLevel : criterion.solves
