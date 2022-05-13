5aff0a1cfc42070d4a8d3b3a9920fb1e1af88541
Jens Andersen <jensandersen@hey.com>
2022-05-13 11:28:54 +0200

    Sort wcag criterias for better overview

--- src/routes/profile/[id].svelte
+++ src/routes/profile/[id].svelte
@@ -7,50 +7,101 @@
 
   export let user
   export let criteria
+  export let sortedWcag
   const level = getWCAGMasterLevel(criteria) // just use the level from the user, when we have made a default for level 0.. until then we +1
   const nextLevel = getNextMasterLevel(level)
-
-  console.log('uauauau')
-  console.log(criteria[0])
 </script>
-
-<!-- markup (zero or more items) goes here -->
 
 <h1>WCAG Master</h1>
 <div class="avatar">
   <div class="w-24 rounded-full">
-    <img src={getWCAGMasterIcon(level + 1)} alt="Icon showing the profiency of WCAG skills" />
+    <img src={getWCAGMasterIcon(level)} alt="Icon showing the profiency of WCAG skills" />
   </div>
 </div>
 
-<!-- DER ER 78 kriterier -->
+<h1 class="pb-6 pt-14 text-3xl">Perceivable criteria</h1>
 <div class="flex flex-wrap gap-6">
-  {#each criteria as criterion}
+  {#each sortedWcag.perceivable as criterion}
     <div
       class="w-50 card flex w-[12.5%] flex-col items-center gap-2 p-2 outline outline-slate-300"
       style:background-color={criterion.principle.color}
+      style:opacity={criterion.solves < nextLevel ? 0.5 : 1}
     >
       <div class="flex">
         <img
-          class="mr-1 h-7 w-7"
+          class="mr-1 h-10 w-10"
           src={getCourseIcon([criterion.principle])}
           alt="The icon of {criterion.principle.name}"
         />
-        <span class="text-xl text-[#0B0A26]">{criterion.number}</span>
+        <span class="pt-1 text-2xl text-[#0B0A26]">{criterion.number}</span>
       </div>
-      <!-- <span
-        title={criterion.principle.description}
-        class="badge badge-lg badge-ghost p-4 text-xl"
-        style:background-color={criterion.principle.color}
-      >
+      <span class="-mb-2 pt-1 text-xs text-[#0B0A26]">Completed:</span>
+      <p class="text-xl text-[#0B0A26]">{criterion.solves} / {nextLevel}</p>
+    </div>
+  {/each}
+</div>
+
+<h1 class="pb-6 pt-14 text-3xl">Operable criteria</h1>
+<div class="flex flex-wrap gap-6">
+  {#each sortedWcag.operable as criterion}
+    <div
+      class="w-50 card flex w-[12.5%] flex-col items-center gap-2 p-2 outline outline-slate-300"
+      style:background-color={criterion.principle.color}
+      style:opacity={criterion.solves < nextLevel ? 0.5 : 1}
+    >
+      <div class="flex">
         <img
-          class="mr-1 h-5 w-5"
+          class="mr-1 h-10 w-10"
           src={getCourseIcon([criterion.principle])}
           alt="The icon of {criterion.principle.name}"
         />
-        {criterion.number}
-      </span> -->
-      <span class="-mb-2 text-xs text-[#0B0A26]">Completed:</span>
+        <span class="pt-1 text-2xl text-[#0B0A26]">{criterion.number}</span>
+      </div>
+      <span class="-mb-2 pt-1 text-xs text-[#0B0A26]">Completed:</span>
+      <p class="text-xl text-[#0B0A26]">{criterion.solves} / {nextLevel}</p>
+    </div>
+  {/each}
+</div>
+
+<h1 class="pb-6 pt-14 text-3xl">Understandable criteria</h1>
+<div class="flex flex-wrap gap-6">
+  {#each sortedWcag.understandable as criterion}
+    <div
+      class="w-50 card flex w-[12.5%] flex-col items-center gap-2 p-2 outline outline-slate-300"
+      style:background-color={criterion.principle.color}
+      style:opacity={criterion.solves < nextLevel ? 0.5 : 1}
+    >
+      <div class="flex">
+        <img
+          class="mr-1 h-10 w-10"
+          src={getCourseIcon([criterion.principle])}
+          alt="The icon of {criterion.principle.name}"
+        />
+        <span class="pt-1 text-2xl text-[#0B0A26]">{criterion.number}</span>
+      </div>
+      <span class="-mb-2 pt-1 text-xs text-[#0B0A26]">Completed:</span>
+      <p class="text-xl text-[#0B0A26]">{criterion.solves} / {nextLevel}</p>
+    </div>
+  {/each}
+</div>
+
+<h1 class="pb-6 pt-14 text-3xl">Robust criteria</h1>
+<div class="flex flex-wrap gap-6">
+  {#each sortedWcag.robust as criterion}
+    <div
+      class="w-50 card flex w-[12.5%] flex-col items-center gap-2 p-2 outline outline-slate-300"
+      style:background-color={criterion.principle.color}
+      style:opacity={criterion.solves < nextLevel ? 0.5 : 1}
+    >
+      <div class="flex">
+        <img
+          class="mr-1 h-10 w-10"
+          src={getCourseIcon([criterion.principle])}
+          alt="The icon of {criterion.principle.name}"
+        />
+        <span class="pt-1 text-2xl text-[#0B0A26]">{criterion.number}</span>
+      </div>
+      <span class="-mb-2 pt-1 text-xs text-[#0B0A26]">Completed:</span>
       <p class="text-xl text-[#0B0A26]">{criterion.solves} / {nextLevel}</p>
     </div>
   {/each}
