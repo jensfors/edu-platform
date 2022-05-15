1cc000ff3702283f8eb8963d79aea5c6a354be07
Jens Andersen <jensandersen@hey.com>
2022-05-15 15:27:46 +0200

    Adjust progressbar in progressCard

--- src/lib/components/progressCard/ProgressCard.svelte
+++ src/lib/components/progressCard/ProgressCard.svelte
@@ -1,64 +1,60 @@
 <script lang="ts">
   import { compliments } from '$lib/utils/compliments'
-  import { onMount } from 'svelte'
-  import { cubicIn } from 'svelte/easing'
+  import { getLevelIcon } from '$lib/utils/levelIcon'
+  import { onDestroy, onMount } from 'svelte'
+  import { sineOut } from 'svelte/easing'
   import { tweened } from 'svelte/motion'
 
   export let user
   let compliment = compliments[Math.floor(Math.random() * compliments.length)]
-  let { nextLevelXP, progressXP } = user.userXP
+  let { nextLevelXP, progressXP, level } = user.userXP
   let progressAfterCompletedExercise = progressXP + 100
+  let progerssPercentage = (progressAfterCompletedExercise / nextLevelXP) * 100 // * 100 to get percentage instead of decimal number
+  let visible = false
+
+  // We wait until the items is visible to animate it, so they are not execute already when the component is mounted
+  $: if (visible) {
+    progress.set(progerssPercentage)
+  }
 
   const progress = tweened(1, {
-    duration: 1000,
-    easing: cubicIn,
+    duration: 2000,
+    easing: sineOut,
   })
 
-  onMount(() => {
-    // sleep function
-    function sleep(ms) {
-      return new Promise((resolve) => setTimeout(resolve, ms))
-    }
-
-    sleep(0).then(() => {
-      progress.set(97)
-    })
-  })
+  console.log('user: ', user)
 </script>
 
 <!-- The button to open modal -->
-<label for="xp-modal" class="modal-button btn">open modal</label>
+<label for="xp-modal" class="modal-button btn" on:click={() => (visible = true)}>open modal</label>
 
 <!-- Put this part before </body> tag -->
 <input type="checkbox" id="xp-modal" class="modal-toggle" />
 <label for="xp-modal" class="modal cursor-pointer">
-  <label class="modal-box relative" for="">
+  <label class="modal-box" for="">
     <h3 class="text-center text-lg font-bold">Congratulations, {compliment}</h3>
     <p class="py-4">
       You've been selected for a chance to get one year of subscription to use Wikipedia for free!
       Put a lot of cool stats in here Xp and stuff
     </p>
-    <div class="flex">
-      <!-- <div class="relative flex justify-center"> -->
-      <div class="meter h-10 w-80">
-        <span style:width="{$progress}%" />
-        <p
-          class="xp left-50% absolute -top-9 w-full text-center text-blue-600"
-          style:--currXP={progressXP}
-          style:--newXP={progressAfterCompletedExercise}
-        />
-      </div>
-      <!-- <progress
-          class="h-10 w-80 appearance-none"
-          style="accent-color: blue;"
-          value={$progress}
-          max="100"
-        /> -->
-      <!-- </div> -->
-      <div class="stats z-50 -mt-3 bg-white shadow">
-        <div class="stat">
-          <div class="stat-value text-2xl">{nextLevelXP}</div>
+    <div class="flex h-20">
+      <div class="avatar z-10">
+        <div class="w-20 rounded-full">
+          <img src={getLevelIcon(level)} alt={'Level icon showing you are level ' + level} />
         </div>
+      </div>
+      {#if visible}
+        <div class="meter top-[25%] -ml-2 h-10 w-80">
+          <span style:width="{$progress}%" />
+          <p
+            class="xp left-50% absolute top-[7px] w-full text-center text-2xl font-extrabold leading-10"
+            style:--currXP={progressXP}
+            style:--newXP={progressAfterCompletedExercise}
+          />
+        </div>
+      {/if}
+      <div class="flex flex-col items-center justify-center rounded-xl border-2 border-solid p-4">
+        <div class="text-4xl font-extrabold">{nextLevelXP}</div>
       </div>
     </div>
   </label>
@@ -72,20 +68,45 @@
     /* margin: 60px 0 20px 0; Just for demo spacing */
     background: #555;
     /* border-radius: 25px; */
-    border-top-right-radius: 8px;
-    border-bottom-right-radius: 8px;
-    border-top-left-radius: 8px;
-    border-bottom-left-radius: 8px;
+    /* border-top-right-radius: 8px; */
+    /* border-bottom-right-radius: 8px; */
+    /* border-top-left-radius: 8px; */
+    /* border-bottom-left-radius: 8px; */
     /* padding: 10px; */
     box-shadow: inset 0 -1px 1px rgba(255, 255, 255, 0.3);
   }
+  .meter::before {
+    content: '';
+    position: absolute;
+    height: 100%;
+    width: 10px;
+    left: -10px;
+    /* border-top-right-radius: 8px; */
+    /* border-bottom-right-radius: 8px; */
+    /* border-top-left-radius: 8px; */
+    /* border-bottom-left-radius: 8px; */
+    /* border-top-left-radius: 20px; */
+    /* border-bottom-left-radius: 20px; */
+    background-color: rgb(43, 194, 83);
+    background-image: linear-gradient(center bottom, rgb(43, 194, 83) 37%, rgb(84, 240, 84) 69%);
+  }
+  /* .meter::after {
+    content: '';
+    position: absolute;
+    height: 25px;
+    width: 25px;
+    background-color: #bbb;
+    border-radius: 50%;
+    display: inline-block;
+  } */
+
   .meter > span {
     display: block;
     height: 100%;
     border-top-right-radius: 8px;
     border-bottom-right-radius: 8px;
-    border-top-left-radius: 8px;
-    border-bottom-left-radius: 8px;
+    /* border-top-left-radius: 8px; */
+    /* border-bottom-left-radius: 8px; */
     /* border-top-left-radius: 20px; */
     /* border-bottom-left-radius: 20px; */
     background-color: rgb(43, 194, 83);
@@ -105,8 +126,8 @@
   .xp {
     animation: counter 3s normal ease-in-out forwards;
     counter-reset: num var(--num);
-    font: 800 38px system-ui;
-    padding: 2rem;
+    font: 800 24px system-ui;
+    /* padding: 2rem; */
   }
   .xp::after {
     content: counter(num);
