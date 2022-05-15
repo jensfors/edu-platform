6d980e7cdc40fdc971efd7aef41867cc646a4bfa
Jens Andersen <jensandersen@hey.com>
2022-05-15 16:24:51 +0200

    Stuff

--- src/routes/course/[courseId]/exercise/[exerciseId].svelte
+++ src/routes/course/[courseId]/exercise/[exerciseId].svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
-  import type { Course, Exercise } from '@prisma/client'
-  import { getCourseIcon } from '$lib/utils/courseIcon'
-  import CodeCell from '$lib/components/editor/CodeCell.svelte'
   import { page } from '$app/stores'
+  import CodeCell from '$lib/components/editor/CodeCell.svelte'
+  import ProgressCard from '$lib/components/progressCard/ProgressCard.svelte'
   import { authUser } from '$lib/stores'
+  import { getCourseIcon } from '$lib/utils/courseIcon'
+  import type { Course, Exercise } from '@prisma/client'
 
   export let exercise: Exercise
   export let course: Course
+
+  console.log('exer: ', exercise)
+  console.log('course: ', course)
 
   let showSolution: boolean = userHasSolvedExercise()
   // @ts-ignore
@@ -58,6 +62,7 @@
   }
 
   async function onSubmit() {
+    console.log('Submitting motherfucker ')
     const res = await fetch(`${$page.url.pathname}`, {
       method: 'POST',
       body: JSON.stringify({
@@ -66,6 +71,7 @@
         difficulty: exercise.difficulty,
       }),
     })
+    console.log('response: ', res)
   }
 
   function userIsAuthor(): boolean {
@@ -91,6 +97,8 @@
   let prevExercise: Exercise = course.exercises[isFirstExercise() ? currentExIndex : prevExIndex]
   // @ts-ignore
   let nextExercise: Exercise = course.exercises[isLastExercise() ? currentExIndex : nextExIndex]
+
+  console.log('authUser', $authUser)
 </script>
 
 <div class="flex justify-center">
@@ -101,10 +109,10 @@
   class="tooltip tooltip-bottom [--tooltip-text-color:black] [--tooltip-color:#fefefe]"
   data-tip={persona.description}
 >
-  <div class="flex p-1 justify-center items-center">
+  <div class="flex items-center justify-center p-1">
     <div class="h-16 w-16">
       <img
-        class="h-full w-auto mask mask-squircle -ml-1"
+        class="mask mask-squircle -ml-1 h-full w-auto"
         src={persona.avatarUrl}
         alt={`Picture for the persona: ${persona.name}`}
       />
@@ -115,12 +123,12 @@
     </div>
   </div>
 </div>
-<div class="pl-8 flex flex-row justify-center">
+<div class="flex flex-row justify-center pl-8">
   {#each criteria as criterion}
     <div class="pr-2 pt-2">
       <span
         title={criterion.criteria.principle.description}
-        class="badge badge-ghost badge-lg"
+        class="badge badge-lg badge-ghost"
         style:background-color={criterion.criteria.principle.color}
       >
         <img
@@ -148,7 +156,7 @@
 <div class="flex justify-center gap-20 py-16">
   {#if isFirstExercise()}
     <a
-      class="btn btn-primary button-width"
+      class="button-width btn btn-primary"
       role="button"
       sveltekit:prefetch
       href={`/course/${course.id}`}>Go back to course</a
@@ -162,7 +170,7 @@
     >
   {/if}
   <button
-    class="btn btn-success button-width"
+    class="button-width btn btn-success"
     disabled={showSolution}
     on:click={() => {
       showSolution = true
@@ -173,10 +181,11 @@
   >
     {showSolution ? 'Answer submitted' : 'Submit answer'}</button
   >
+  <!-- <ProgressCard /> -->
   <div>
     {#if isLastExercise()}
       <a
-        class="btn btn-primary button-width"
+        class="button-width btn btn-primary"
         role="button"
         sveltekit:prefetch
         href={`/course/${course.id}`}>Go back to course</a
--- src/routes/course/[courseId]/exercise/[exerciseId].ts
+++ src/routes/course/[courseId]/exercise/[exerciseId].ts
@@ -1,7 +1,8 @@
 import { getCourseFromExercise, getExercise, userSolvesExercise } from '$lib/db/exercises'
-import { getDifficultyXP } from '$lib/utils/levels'
-import type { Course, Exercise } from '@prisma/client'
-
+import { getUser, getXP } from '$lib/db/user'
+import { getDifficultyXP, getUserLevel } from '$lib/utils/levels'
+import type { XP } from '$lib/utils/stringTypes'
+import type { Course, Exercise, User } from '@prisma/client'
 
 export async function get({ params }) {
   const { exerciseId } = params
@@ -21,12 +22,24 @@ export async function get({ params }) {
 }
 
 export async function post({ request }) {
+  // Insert new post data in DB
   let data = await request.json()
   let xp = getDifficultyXP(data.difficulty)
   let result = await userSolvesExercise(data.exerciseId, data.userId, xp)
+
+  // Fetch data for xp modal
+  const user: User = await getUser(data.userId)
+  const totalXP: number = await getXP(user)
+  const userXP: XP = getUserLevel(totalXP)
+  // @ts-ignore
+  userXP.totalXP = totalXP
+  // @ts-ignore
+  user.userXP = userXP
+
   return {
     body: {
       message: result ? 'Exercise successfully solved' : 'Damn, something went kinda wrong',
+      user,
     },
   }
 }
