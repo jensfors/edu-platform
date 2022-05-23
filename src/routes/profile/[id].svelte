edf09f320714a9ff6def7f8edf543a611b17a4df
Mathias Rasmussen <34272381+MathiasAAU@users.noreply.github.com>
2022-05-23 13:19:47 +0200

    Profile page upgrade (#44)
    * Made awards a component
    
    * Text formatting
    
    * Courses and personas on profile
    
    * More Profile page
    
    * Clean up
    
    * Profile only shows courses/personas if they have any
    
    * Changed logic a bit

--- src/routes/profile/[id].svelte
+++ src/routes/profile/[id].svelte
@@ -1,136 +1,86 @@
 <script lang="ts">
-  // your script goes here
+  import AwardCollapse from '$lib/components/awards/AwardCollapse.svelte'
   import ProgressCard from '$lib/components/progressCard/ProgressBar.svelte'
+  import Verified from '$lib/components/Verified.svelte'
+  import ContentCollapse from '$lib/components/ContentCollapse.svelte'
   import { authUser } from '$lib/stores'
-  import { getNextMasterLevel, getWCAGMasterLevel } from '$lib/utils/awards'
-  import { getCourseIcon } from '$lib/utils/courseIcon'
-  import { getMasterColor, getWCAGMasterIcon } from '$lib/utils/levelIcon'
+  import { getPrincipleMasterLevel, getWCAGMasterLevel } from '$lib/utils/awards'
+  import { formatDate } from '$lib/utils/stringFormating'
   import { getPrincipleList, Principle, Roles } from '$lib/utils/stringTypes'
+  import { getPrincipleMasterIcon, getWCAGMasterIcon } from '$lib/utils/levelIcon'
 
   export let user
   export let criteria
   export let sortedCriteria
+  const principleList: Principle[] = getPrincipleList()
   let wcagMasterLevel = getWCAGMasterLevel(criteria)
-  const nextWCAGMasterLevel = getNextMasterLevel(wcagMasterLevel)
-
-  let visible = false
-
-  // Returns the criteria for a principle
-  function getPrincipleCriteria(principle: Principle) {
-    switch (principle) {
-      case Principle.Perceivable:
-        return sortedCriteria.perceivable
-      case Principle.Operable:
-        return sortedCriteria.operable
-      case Principle.Understandable:
-        return sortedCriteria.understandable
-      case Principle.Robust:
-        return sortedCriteria.robust
-    }
-  }
 </script>
 
 <!-- Create course button -->
 {#if $authUser.id === user.id && ($authUser?.role === Roles.Creator || $authUser?.role === Roles.Admin)}
-  <div class="flex justify-center pb-8">
+  <div class="flex justify-center pb-4">
     <a class="button-width btn btn-primary" role="button" sveltekit:prefetch href={`/create-course`}
       >Create course</a
     >
   </div>
 {/if}
 
-<div class="flex justify-center">
-  <ProgressCard {user} />
-</div>
-<div class="divider" />
-
-<!-- WCAG Master Collapse -->
-<div tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
-  <!-- Header -->
-  <input type="checkbox" checked />
-  <div class="flex collapse-title bg-primary p-2 pl-4">
-    <div class="w-14 rounded-full">
+<!-- Profile information section -->
+<div class="flex flex-col justify-center items-center">
+  <div class="">
+    <ProgressCard {user} />
+  </div>
+  <div class="flex p-4">
+    <div class="h-16 w-16">
       <img
-        src={getWCAGMasterIcon(wcagMasterLevel)}
-        alt="Icon showing the profiency of WCAG skills"
+        class="h-full w-auto mask mask-squircle -ml-1"
+        src={user.avatarURL}
+        alt={`Profile picture for the author: ${user.firstName} ${user.lastName}`}
       />
     </div>
-    <h1 class="flex items-center text-2xl font-medium pl-3">WCAG Master</h1>
+    <div class="flex flex-col items-center pl-1">
+      <h1 class="text-3xl">
+        {`${user.firstName} ${user.lastName}`}
+        <Verified role={user.role} />
+      </h1>
+
+      <p><i>Account created {formatDate(user.createdAt)}</i></p>
+    </div>
   </div>
-  <!-- Content -->
-  <div class="collapse-content">
-    <div class="flex justify-center items-center">
-      <div class="avatar">
-        <div class="w-24 rounded-full">
+  <div class="flex flex-row gap-4 pt-2">
+    <!-- Show all master level badges-->
+    {#each principleList as principle, index}
+      <!-- Show WCAG Master level in the middle-->
+      {#if principleList.length / 2 === index}
+        <div class="w-20 rounded-full">
           <img
             src={getWCAGMasterIcon(wcagMasterLevel)}
-            alt="Icon showing the profiency of WCAG skills"
+            alt={`Icon showing the WCAG Master level`}
           />
         </div>
-      </div>
-      <div class="meter top-[25%] h-10 w-80 border-solid border-2 border-black">
-        <span class="" style:width="{80}%" style:background={getMasterColor(wcagMasterLevel)} />
-        <p
-          class="left-50% absolute top-[0px] w-full text-center text-2xl font-extrabold leading-10"
-        >
-          processText
-        </p>
-      </div>
-    </div>
-
-    {#each getPrincipleList() as principle}
-      <div class="divider" />
-      <h2 class="pb-6 text-xl">{principle} criteria</h2>
-      <div class="flex flex-wrap gap-6">
-        {#each getPrincipleCriteria(principle) as criterion}
-          <div
-            class="w-50 card flex w-[12.5%] flex-col items-center gap-2 p-2 outline outline-slate-300"
-            style:background-color={criterion.principle.color}
-            style:opacity={criterion.solves < nextWCAGMasterLevel ? 0.5 : 1}
-          >
-            <div class="flex">
-              <img
-                class="mr-1 h-10 w-10"
-                src={getCourseIcon([criterion.principle])}
-                alt="The icon of {criterion.principle.name}"
-              />
-              <span class="pt-1 text-2xl text-[#0B0A26]">{criterion.number}</span>
-            </div>
-            <span class="-mb-2 pt-1 text-xs text-[#0B0A26]">Completed:</span>
-            <p class="text-xl text-[#0B0A26]">{criterion.solves} / {nextWCAGMasterLevel}</p>
-          </div>
-        {/each}
+      {/if}
+      <div class="flex rounded-full w-16 items-center">
+        <img
+          src={getPrincipleMasterIcon(getPrincipleMasterLevel(criteria, principle), principle)}
+          alt={`Icon showing the ${principle} Master level`}
+        />
       </div>
     {/each}
   </div>
+  <div class="divider" />
 </div>
 
-<!-- <div class="w-30">
-  <ProgressCardModal {user} />
-</div> -->
-<style>
-  .meter {
-    box-sizing: content-box;
-    /* height: 20px; Can be anything */
-    position: relative;
-    /* margin: 60px 0 20px 0; Just for demo spacing */
-    background: rgb(206, 206, 206);
-    /* border-radius: 25px; */
-    /* border-top-right-radius: 8px; */
-    /* border-bottom-right-radius: 8px; */
-    /* border-top-left-radius: 8px; */
-    /* border-bottom-left-radius: 8px; */
-    /* padding: 10px; */
-    box-shadow: inset 0 -1px 1px rgba(255, 255, 255, 0.3);
-  }
+<ContentCollapse {user} type={'Personas'} />
+<ContentCollapse {user} type={'Courses'} />
 
-  .meter > span {
-    display: block;
-    height: 100%;
-    /* border-top-left-radius: 8px; */
-    /* border-bottom-left-radius: 8px; */
-    /* border-top-left-radius: 20px; */
-    /* border-bottom-left-radius: 20px; */
-    background-image: linear-gradient(center bottom, rgb(43, 194, 83) 37%, rgb(84, 240, 84) 69%);
-  }
-</style>
+<!-- WCAG Master Collapse -->
+<AwardCollapse {sortedCriteria} currentLevel={wcagMasterLevel} principle={null} />
+
+<!-- All Principle Master Collapses -->
+{#each principleList as principle}
+  <AwardCollapse
+    {sortedCriteria}
+    currentLevel={getPrincipleMasterLevel(criteria, principle)}
+    {principle}
+  />
+{/each}
