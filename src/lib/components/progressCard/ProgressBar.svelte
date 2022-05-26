4ecb5b70ddf8233b84d1098db321ab90419d9c01
Jens Andersen <jensandersen@hey.com>
2022-05-26 12:10:56 +0200

    Refactored progressbar

--- src/lib/components/progressCard/ProgressBar.svelte
+++ src/lib/components/progressCard/ProgressBar.svelte
@@ -1,50 +1,132 @@
 <script lang="ts">
-  import { compliments } from '$lib/utils/compliments'
   import { getLevelBorderColor, getLevelColor, getLevelIcon } from '$lib/utils/levelIcon'
-  import { onDestroy, onMount } from 'svelte'
+  import confetti from 'canvas-confetti'
   import { sineOut } from 'svelte/easing'
   import { tweened } from 'svelte/motion'
+  import { fade } from 'svelte/transition'
 
-  export let user
-  let compliment = compliments[Math.floor(Math.random() * compliments.length)]
-  let { nextLevelXP, progressXP, level } = user.userXP
-  let progressAfterCompletedExercise = progressXP
-  let progerssPercentage = (progressAfterCompletedExercise / nextLevelXP) * 100 // * 100 to get percentage instead of decimal number
-  let visible = false
+  interface userXP {
+    afterXP: {
+      level: number
+      nextLevelXP: number
+      progressXP: number
+    }
+    beforeXP: {
+      level: number
+      nextLevelXP: number
+      progressXP: number
+    }
+  }
+  export let userXP: userXP
 
-  // We wait until the items is visible to animate it, so they are not execute already when the component is mounted
+  // destructuring props
+  let {
+    level: beforeLevel,
+    nextLevelXP: beforeTotalXP,
+    progressXP: beforeProgressXP,
+  } = userXP.beforeXP
+  let { level: afterLevel, nextLevelXP: afterTotalXP, progressXP: afterProgressXP } = userXP.afterXP
 
+  let initialBarXP = beforeProgressXP
+  let afterBarXP = afterProgressXP
+  let barPercentage = (afterTotalXP / beforeTotalXP) * 100 // * 100 to get percentage instead of decimal number
+  let remainingXPToNextLevel = afterTotalXP - afterProgressXP
+
+  // Animation variables
   const progress = tweened(1, {
     duration: 2000,
     easing: sineOut,
   })
 
-  progress.set(progerssPercentage)
-  console.log('user: ', user)
+  const barXP = tweened(1, {
+    duration: 2000,
+    easing: sineOut,
+  })
+
+  const remainingXP = tweened(1, {
+    duration: 2000,
+    easing: sineOut,
+  })
+
+  console.log('yaya ', beforeTotalXP, beforeProgressXP, beforeLevel)
+  console.log('userXP: ', userXP)
+  console.log('Remaining ', remainingXPToNextLevel)
+
+  console.log('before: ', beforeTotalXP)
+  console.log('after: ', afterTotalXP)
+  console.log('barPercentage: ', barPercentage)
+
+  animate()
+
+  function animate() {
+    const maxPercentage = 100
+    // Re-animate if they level up
+    if (afterLevel > beforeLevel) {
+      // PAAAAAARTY - they leveled up for gods sake!
+      confetti({
+        particleCount: 250,
+        spread: 100,
+        disableForReducedMotion: true,
+      })
+
+      // Animate remaining of current lvl xp
+      progress.set(maxPercentage)
+      barXP.set(beforeTotalXP)
+      remainingXP.set(beforeTotalXP)
+
+      // timeout to wait until the first animation is done
+      setTimeout(() => {
+        // Reset state
+        progress.set(0, { duration: 0 })
+        barXP.set(0, { duration: 0 })
+        remainingXP.set(0, { duration: 0 })
+        // Animate with new values
+        progress.set(barPercentage - maxPercentage)
+        barXP.set(afterProgressXP)
+        remainingXP.set(remainingXPToNextLevel)
+      }, 2000)
+    } else {
+      progress.set(barPercentage)
+      console.log('remainingXP: ', remainingXPToNextLevel)
+      remainingXP.set(remainingXPToNextLevel)
+      console.log('fuck me daddy: ', afterProgressXP)
+      barXP.set(afterProgressXP)
+    }
+  }
 </script>
 
-<div class="flex h-20">
+<div class="flex h-full w-full" in:fade>
+  <!-- Level Icon -->
   <div class="avatar z-10">
     <div class="w-20 rounded-full">
-      <img src={getLevelIcon(level)} alt={'Level icon showing you are level ' + level} />
+      <img
+        src={getLevelIcon(beforeLevel)}
+        alt={'Level icon showing you are level ' + beforeLevel}
+      />
     </div>
   </div>
-
+  <!-- Progress Bar -->
   <div class="meter top-[25%] -ml-2 h-10 w-80">
     <span style:width="{$progress}%" />
-    <p
+    <p class="left-50% absolute top-[1px] w-full text-center text-2xl font-extrabold leading-10">
+      {$barXP.toFixed(0)}
+    </p>
+    <!-- <p
       class="xp left-50% absolute top-[7px] w-full text-center text-2xl font-extrabold leading-10"
-      style:--currXP={progressXP}
-      style:--newXP={progressAfterCompletedExercise}
-    />
+      style:--currXP={initialBarXP}
+      style:--newXP={afterBarXP}
+    /> -->
   </div>
-
+  <!-- Remaining XP to level up -->
   <div
     class="flex flex-col items-center justify-center rounded-xl border-2 border-solid p-4"
-    style:background-color={getLevelColor(level + 1)}
-    style:border-color={getLevelBorderColor(level + 1)}
+    style:background-color={getLevelColor(beforeLevel + 1)}
+    style:border-color={getLevelBorderColor(beforeLevel + 1)}
   >
-    <div class="text-4xl font-extrabold">{nextLevelXP}</div>
+    <div class="stat-desc opacity-100">XP to level up</div>
+    <div class="text-4xl font-extrabold">
+      {$remainingXP.toFixed(0)}
+    </div>
   </div>
 </div>
 
@@ -99,34 +181,5 @@
     /* border-bottom-left-radius: 20px; */
     background-color: rgb(43, 194, 83);
     background-image: linear-gradient(center bottom, rgb(43, 194, 83) 37%, rgb(84, 240, 84) 69%);
-  }
-
-  .yaya {
-    font: 800 40px system-ui;
-  }
-
-  @property --num {
-    syntax: '<integer>';
-    initial-value: 0;
-    inherits: false;
-  }
-
-  .xp {
-    animation: counter 3s normal ease-in-out forwards;
-    counter-reset: num var(--num);
-    font: 800 24px system-ui;
-    /* padding: 2rem; */
-  }
-  .xp::after {
-    content: counter(num);
-  }
-
-  @keyframes counter {
-    from {
-      --num: var(--currXP);
-    }
-    to {
-      --num: var(--newXP);
-    }
   }
 </style>
